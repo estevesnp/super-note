ba468a570c2e0296973a94512f8961b7aea98981
estevesnp <nuno.pinto@live.com>
2025-01-08 23:04:46 +0000

    added fe routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,19 @@
-import type { Component } from 'solid-js';
+import type { JSX } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
-const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+type AppProps = {
+  children?: JSX.Element;
 };
+
+function App({ children }: AppProps) {
+  return (
+    <>
+      <header class={styles.header}>header</header>
+      {children}
+      <footer class={styles.footer}>footer</footer>
+    </>
+  );
+}
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
+import { Router, Route } from "@solidjs/router";
+import App from "./App";
+import Home from "./Home";
 
-import './index.css';
-import App from './App';
+const root = document.getElementById("root");
 
-const root = document.getElementById('root');
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
-  );
+if (!root) {
+  throw new Error("Root element not found.");
 }
 
-render(() => <App />, root!);
+render(
+  () => (
+    <Router root={App}>
+      <Route path="/" component={Home} />
+    </Router>
+  ),
+  root,
+);
